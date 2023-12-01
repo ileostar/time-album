5a204a4b6a994cc1b01539c454e56478f5c3a26b
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-12-01 22:14:58 +0800

    chore: finish user page

--- src/pages/me.vue
+++ src/pages/me.vue
@@ -1,51 +1,63 @@
+<script setup lang="ts">
+const visible1 = ref(false)
+function onCancel() {
+  // eslint-disable-next-line no-console
+  console.log('event cancel')
+}
+function onOk() {
+  // eslint-disable-next-line no-console
+  console.log('event ok')
+}
+function baseClick(): void {
+  visible1.value = true
+}
+</script>
+
 <template>
   <view flex="~ col" box-border h-full w-full>
-    <view relative box-border h-30>
+    <view mt-2 light:bg="gray-200/30" dark:bg="gray" box-border h-30 flex items-center justify-between gap-5 rd-5 px-5 shadow-sm>
       <nut-avatar z-1 -m-20 size="large">
         <img src="https://img12.360buyimg.com/imagetools/jfs/t1/196430/38/8105/14329/60c806a4Ed506298a/e6de9fb7b8490f38.png">
       </nut-avatar>
-      <view border-box relative h-full w-full rd-5 bg="gray-200/80" dark:bg="gray" pt-10 -z-1 -mt-10 blur="0.5">
-        <p color="#000">
-          name
+      <view flex="~ col" box-border flex-1 items-start>
+        <p text-5 font-bold>
+          LeoStar
         </p>
-        <p color="#000">
-          name
+        <p text-3 color-gray-5>
+          青铜
         </p>
       </view>
+      <span i-carbon:chevron-right text-7 color-gray @click="baseClick" />
     </view>
-    <view mt-20 box-border flex-1 rd-5>
-      <nut-cell title="图片" desc="描述文字">
-        <template #icon>
-          <img
-            class="nut-icon"
-            src="https://img11.360buyimg.com/imagetools/jfs/t1/137646/13/7132/1648/5f4c748bE43da8ddd/a3f06d51dcae7b60.png"
-          >
-        </template>
-      </nut-cell>
-      <nut-cell title="图片" desc="描述文字">
-        <template #icon>
-          <img
-            class="nut-icon"
-            src="https://img11.360buyimg.com/imagetools/jfs/t1/137646/13/7132/1648/5f4c748bE43da8ddd/a3f06d51dcae7b60.png"
-          >
-        </template>
-      </nut-cell>
-      <nut-cell title="图片" desc="描述文字">
-        <template #icon>
-          <img
-            class="nut-icon"
-            src="https://img11.360buyimg.com/imagetools/jfs/t1/137646/13/7132/1648/5f4c748bE43da8ddd/a3f06d51dcae7b60.png"
-          >
-        </template>
-      </nut-cell>
-      <nut-cell title="图片" desc="描述文字">
-        <template #icon>
-          <img
-            class="nut-icon"
-            src="https://img11.360buyimg.com/imagetools/jfs/t1/137646/13/7132/1648/5f4c748bE43da8ddd/a3f06d51dcae7b60.png"
-          >
-        </template>
-      </nut-cell>
-    </view>
+    <div mt-3 h-10 flex items-center>
+      <span h-5 w-1 bg-blue />
+      <h3 ml-3 font-bold>
+        智能图像
+      </h3>
+    </div>
+    <div mt-1 h-30 w-full flex gap-2 overflow-hidden>
+      <div class="people" flex="~ 1" w="50%" relative h-full items-end justify-center overflow-hidden rd-3 shadow-sm blur="0.5">
+        <h3 mb-2 color-white>
+          人物
+        </h3>
+      </div>
+      <div class="landscape" flex="~ 1" w="50%" relative h-full items-end justify-center overflow-hidden rd-3 shadow-sm blur="0.5">
+        <h3 mb-2 color-white>
+          风景
+        </h3>
+      </div>
+    </div>
+    <nut-dialog v-model:visible="visible1" title="基础弹框" content="这是基础弹框。" @cancel="onCancel" @ok="onOk" />
   </view>
 </template>
+
+<style>
+.people {
+  background-image: url('https://ts1.cn.mm.bing.net/th/id/R-C.57170d3d0875233cb3ba434e61a8d0ec?rik=xIElgarHgUfmiA&riu=http%3a%2f%2fsc.68design.net%2fphotofiles%2f201901%2f3IME6jvHWl.jpg&ehk=O0xKV8MOnNRn18PapEoKmsftrBNGmgscWfM7ExczdDA%3d&risl=&pid=ImgRaw&r=0');
+  background-size: cover;
+}
+.landscape {
+  background-image: url('https://ts1.cn.mm.bing.net/th/id/R-C.3e42ad7c7744a7a612bfbaa480405611?rik=eX46Dun%2fFF1aDg&riu=http%3a%2f%2fpic.bizhi360.com%2fbbpic%2f25%2f6125.jpg&ehk=ukEQFHgNgClnZxjkosi%2bP3OoaZicxvcitCFcgJISxww%3d&risl=&pid=ImgRaw&r=0');
+  background-size: cover;
+}
+</style>
