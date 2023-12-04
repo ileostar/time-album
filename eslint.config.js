46c1f812a1914472df1dfab9814d82ed666c5b65
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-12-04 20:57:11 +0800

    fix: solve eslint error

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ const uni = require('@uni-helper/eslint-config')
 const unocss = require('@unocss/eslint-plugin')
 
 module.exports = uni(
-  {},
+  {
+    ignorePatterns: ['**/helper/.ts'],
+  },
   unocss.configs.flat,
 )
--- src/store/footer.ts
+++ src/store/footer.ts
@@ -1,4 +1,4 @@
-export const useFooterStore = defineStore('footer', () => {
+export default defineStore('footer', () => {
   /***
    * 当前选中菜单的索引，默认为第一个菜单
    */
