cba469f6a0b0bd46b83b4b1f13eee72d23c7b4b7
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-12-05 19:47:47 +0800

    style: finish search color

--- src/layouts/searchPage.vue
+++ src/layouts/searchPage.vue
@@ -1,7 +1,7 @@
 <template>
   <view flex="~ col" box-border h-full w-full justify-between px-5 text-center>
     <nut-sticky>
-      <nut-searchbar>
+      <nut-searchbar background="transparent" input-background="rgba(125,125,125,0.15)">
         <template #leftin>
           <nut-icon name="search2" />
         </template>
